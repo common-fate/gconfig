ca53cb14025caf27ae1c27a4d3a728084c119059
Chris Norman <17420369+chrnorm@users.noreply.github.com>
2021-12-14 18:35:00 +0000

    add config diffing

---
+++ diff.go
@@ -0,0 +1,131 @@
+package gconfig
+
+import (
+	"fmt"
+	"reflect"
+)
+
+type Changes struct {
+	DeleteProviders []string
+	AddProviders    []Provider
+	DeleteUsers     []string
+	AddUsers        []string
+	UpdateUsers     []UpdateUser
+	DeleteAdmins    []string
+	AddAdmins       []string
+}
+
+type UpdateUser struct {
+	Email string
+	// whether to make the user an admin or not
+	Admin bool
+}
+
+type ErrNoInPlaceUpdates struct {
+	Type string
+	ID   string
+}
+
+func (e *ErrNoInPlaceUpdates) Error() string {
+	return fmt.Sprintf("Granted doesn't yet support in-place updates for %s %s", e.Type, e.ID)
+}
+
+func (c *Config) ChangesFrom(old Config) (Changes, error) {
+	var ch Changes
+
+	oldProvidersToDelete := make(map[string]Provider)
+	newProvidersToAdd := make(map[string]Provider)
+
+	for _, p := range old.Providers {
+		oldProvidersToDelete[p.ID] = p
+	}
+
+	for _, p := range c.Providers {
+		if old, ok := oldProvidersToDelete[p.ID]; ok {
+			// resource is common between old and new
+
+			if !reflect.DeepEqual(old, p) {
+				return Changes{}, &ErrNoInPlaceUpdates{Type: "provider", ID: p.ID}
+			}
+
+			delete(oldProvidersToDelete, p.ID)
+		} else {
+			// resource is new
+			newProvidersToAdd[p.ID] = p
+			ch.AddProviders = append(ch.AddProviders, p)
+		}
+	}
+
+	for id := range oldProvidersToDelete {
+		ch.DeleteProviders = append(ch.DeleteProviders, id)
+	}
+
+	// user diffing
+	// the full pool of users is both admins and users.
+	// a user could:
+	// 1. add a new user
+	// 2. delete a user
+	// 3. update a user (change them from a user to an admin, or vice versa)
+	type userDetails struct {
+		IsAdmin bool
+	}
+
+	oldUsersToDelete := make(map[string]userDetails)
+	allNewUsers := make(map[string]userDetails)
+
+	for _, u := range old.Users {
+		oldUsersToDelete[u.Email] = userDetails{IsAdmin: false}
+	}
+
+	for _, u := range old.Admins {
+		oldUsersToDelete[u.Email] = userDetails{IsAdmin: true}
+	}
+
+	// combine admins and users into one list
+	for _, u := range c.Users {
+		allNewUsers[u.Email] = userDetails{IsAdmin: false}
+	}
+	for _, u := range c.Admins {
+		allNewUsers[u.Email] = userDetails{IsAdmin: true}
+	}
+
+	for email, new := range allNewUsers {
+		if old, ok := oldUsersToDelete[email]; ok {
+			// user is common between old and new, so don't delete them
+			if old.IsAdmin != new.IsAdmin {
+				// user has been updated
+				ch.UpdateUsers = append(ch.UpdateUsers, UpdateUser{
+					Email: email,
+					Admin: new.IsAdmin,
+				})
+			}
+			delete(oldUsersToDelete, email)
+
+		} else {
+			// user is new
+			if new.IsAdmin {
+				ch.AddAdmins = append(ch.AddAdmins, email)
+			} else {
+				ch.AddUsers = append(ch.AddUsers, email)
+			}
+		}
+	}
+
+	for email, u := range oldUsersToDelete {
+		if u.IsAdmin {
+			ch.DeleteAdmins = append(ch.DeleteAdmins, email)
+		} else {
+			ch.DeleteUsers = append(ch.DeleteUsers, email)
+		}
+	}
+
+	return ch, nil
+}
+
+// checkFieldEqual returns an formatted error if a != b
+func checkFieldEqual(resourcetype, id, field, a, b string) error {
+	if a != b {
+		return fmt.Errorf("Tried to change %s %s %s from %s to %s but Granted doesn't yet support in-place updates. Delete the %s and then add a new one.", resourcetype, id, field, a, b, resourcetype)
+	}
+	return nil
+}
---
+++ diff_test.go
@@ -0,0 +1,156 @@
+package gconfig
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestDiff(t *testing.T) {
+	old := Config{
+		Providers: []Provider{
+			{
+				ID:   "first",
+				Type: "first",
+			},
+			{
+				ID:   "common",
+				Type: "common",
+			},
+		},
+	}
+
+	new := Config{
+		Providers: []Provider{
+			{
+				ID:   "second",
+				Type: "second",
+			},
+			{
+				ID:   "common",
+				Type: "common",
+			},
+		},
+	}
+
+	res, err := new.ChangesFrom(old)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	expected := Changes{
+		DeleteProviders: []string{"first"},
+		AddProviders: []Provider{
+			{
+				ID:   "second",
+				Type: "second",
+			},
+		},
+	}
+
+	assert.Equal(t, expected, res)
+}
+
+func TestUserDiff(t *testing.T) {
+	old := Config{
+		Users: []Member{
+			{
+				Email: "common@test.com",
+			},
+			{
+				Email: "old@test.com",
+			},
+		},
+	}
+
+	new := Config{
+		Users: []Member{
+			{
+				Email: "common@test.com",
+			},
+			{
+				Email: "new@test.com",
+			},
+		},
+	}
+
+	res, err := new.ChangesFrom(old)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	expected := Changes{
+		DeleteUsers: []string{"old@test.com"},
+		AddUsers:    []string{"new@test.com"},
+	}
+
+	assert.Equal(t, expected, res)
+}
+
+func TestUpdateUserDiff(t *testing.T) {
+	old := Config{
+		Users: []Member{
+			{
+				Email: "common@test.com",
+			},
+			{
+				Email: "old@test.com",
+			},
+		},
+	}
+
+	new := Config{
+		Admins: []Member{
+			{
+				Email: "old@test.com",
+			},
+		},
+		Users: []Member{
+			{
+				Email: "common@test.com",
+			},
+		},
+	}
+
+	res, err := new.ChangesFrom(old)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	expected := Changes{
+		UpdateUsers: []UpdateUser{
+			{
+				Email: "old@test.com",
+				Admin: true,
+			},
+		},
+	}
+
+	assert.Equal(t, expected, res)
+}
+
+// Currently we don't support in-place updates for resoureces like
+// providers and accounts - instead we return an error so the user
+// knows they need to create a new provider with a different ID.
+func TestUpdateProviderDiff(t *testing.T) {
+	old := Config{
+		Providers: []Provider{
+			{
+				ID:   "first",
+				Type: "first",
+			},
+		},
+	}
+
+	new := Config{
+		Providers: []Provider{
+			{
+				ID:   "first",
+				Type: "second",
+			},
+		},
+	}
+
+	_, err := new.ChangesFrom(old)
+	assert.Equal(t, err, &ErrNoInPlaceUpdates{Type: "provider", ID: "first"})
+}
