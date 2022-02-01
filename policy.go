1a0dd4d2dddee66fba29a019ad09a894ab7a9063
Joshua Wilkes <14214200+JoshuaWilkes@users.noreply.github.com>
2022-02-01 08:57:47 +0800

    Add rule selector method

--- policy.go
+++ policy.go
@@ -1,10 +1,63 @@
 package gconfig
 
+import (
+	"crypto/x509"
+	"sort"
+
+	gconfigv1alpha1 "github.com/common-fate/gconfig/gen/gconfig/v1alpha1"
+)
+
 type RulePolicy int
 
+// The order of this enumeration should represent the order of preference for a developer
+// e.g when assuming a role, which has multiple rules, the rule with assume will be selected before requireReason etc
 //go:generate go run github.com/alvaroloes/enumer -type=RulePolicy -linecomment
 const (
 	RulePolicyAllow           RulePolicy = iota + 1 // allow
-	RulePolicyRequireApproval                       // requireApproval
 	RulePolicyRequireReason                         // requireReason
+	RulePolicyRequireApproval                       // requireApproval
 )
+
+// This function is to be used by all services where there is a need to select a rule to apply from a list of rules
+// For added security, this function takes in the user certificate to ensure that the groups match the rules
+// The certificate should be validated before being used with this method
+func RuleSelector(cert *x509.Certificate, rulesInput []*gconfigv1alpha1.Rule) *gconfigv1alpha1.Rule {
+	admin := false
+	groups := cert.Subject.OrganizationalUnit
+	for _, group := range groups {
+		if group == "granted:administrators" {
+			admin = true
+		}
+	}
+	rules := []*gconfigv1alpha1.Rule{}
+	if admin {
+		rules = rulesInput
+	} else {
+		for _, rule := range rulesInput {
+			for _, group := range groups {
+				if group == rule.Group {
+					rules = append(rules, rule)
+				}
+			}
+		}
+	}
+
+	if len(rules) == 0 {
+		return nil
+	}
+	sort.Slice(rules, func(i, j int) bool {
+		// error should never happen here
+		a, err := RulePolicyString(rules[i].Policy)
+		if err != nil {
+			return false
+		}
+		// error should never happen here
+		b, err := RulePolicyString(rules[j].Policy)
+		if err != nil {
+			return false
+		}
+		// uses teh ordering of the ENUM to determin order of rules
+		return a < b
+	})
+	return rules[0]
+}
---
+++ policy_test.go
@@ -0,0 +1,23 @@
+package gconfig
+
+import (
+	"crypto/x509"
+	"crypto/x509/pkix"
+	"testing"
+
+	gconfigv1alpha1 "github.com/common-fate/gconfig/gen/gconfig/v1alpha1"
+	"github.com/stretchr/testify/assert"
+)
+
+// Tests the the correct ordering of rules is observed
+func Test_RuleSelector(t *testing.T) {
+	cert := &x509.Certificate{Subject: pkix.Name{OrganizationalUnit: []string{"developer", "tester"}}}
+	rules := []*gconfigv1alpha1.Rule{{Policy: RulePolicyRequireReason.String(), Group: "developer"}, {Policy: RulePolicyAllow.String(), Group: "tester"}, {Policy: RulePolicyRequireApproval.String(), Group: "reasonNeeders"}}
+	rule := RuleSelector(cert, rules)
+	assert.Equal(t, rules[1], rule)
+
+	// user does not have tester group so the developer rule is retured
+	cert = &x509.Certificate{Subject: pkix.Name{OrganizationalUnit: []string{"developer"}}}
+	rule = RuleSelector(cert, rules)
+	assert.Equal(t, rules[0], rule)
+}
