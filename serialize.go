a1add9105f167601e205e188b4a22ad0d4483993
meyerjrr <jrmeyer339@gmail.com>
2022-03-28 10:39:29 +1100

    added to protobuf serialize

--- serialize.go
+++ serialize.go
@@ -118,8 +118,9 @@ func FromProtobuf(c *gconfigv1alpha1.Config, providers *gconfigv1alpha1.Provider
 		}
 		for _, rule := range r.Rules {
 			role.Rules = append(role.Rules, Rule{
-				Policy: RulePolicyField{Policy: rule.Policy},
-				Group:  rule.Group,
+				Policy:            RulePolicyField{Policy: rule.Policy},
+				Group:             rule.Group,
+				RequireJiraTicket: rule.Token,
 
 				Breakglass: rule.Breakglass,
 			})
